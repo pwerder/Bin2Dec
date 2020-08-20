ced6038b91480e70004a9875b1a9d90161346e67
Paulo Antonio <paulo.werder@gmail.com>
2020-08-20 01:16:30 -0300

    colocando erro de string

--- script.php
+++ script.php
@@ -3,17 +3,23 @@
 $binario = $_POST['binario'];
 $arr = str_split($binario);
 
-
-if(empty($binario))
-{
-    echo "O campo não pode estar vazio";
-    return;
-}
 if(count($arr) > 8)
 {
     echo "O binario deve conter 8 digitos";
     return;
 }
+if(empty($binario))
+{
+    echo "O campo não pode estar vazio";
+    return;
+}
+if(!is_numeric($binario))
+{
+    echo "Só aceita 0 e 1";
+    return;
+}
+
+
 
 $decimal = 0;
 $bin = array_reverse($arr);
